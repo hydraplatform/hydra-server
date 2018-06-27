a3e72aeb2581dbceae367152bf6e297da45064dd
Giovanni Basolu <giovanni.basolu@gmail.com>
2018-06-27 17:07:48 +0100

    add virtualenv

--- pm2_startup.sh
+++ pm2_startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo $ROOT_HYDRA_FOLDER
-. ~/venv3/bin/activate
-~/venv3/bin/python -u $ROOT_HYDRA_FOLDER/hydra-server/run_server.py
+. ~/VirtualEnvs/venv3/bin/activate
+~/VirtualEnvs/venv3/bin/python -u $ROOT_HYDRA_FOLDER/hydra-server/run_server.py
