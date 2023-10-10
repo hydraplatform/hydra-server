64fe570b17ae31004a682caaa10f801361dbd2af
Stephen Knox <sknox@nexsys-analytics.com>
2023-10-10 16:14:33 +0100

    Add anydict import

--- hydra_server/server/scenario.py
+++ hydra_server/server/scenario.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with HydraPlatform.  If not, see <http://www.gnu.org/licenses/>
 #
-from spyne.model.primitive import Integer, Integer32, Unicode
+from spyne.model.primitive import Integer, Integer32, Unicode, AnyDict
 from spyne.model.complex import Array as SpyneArray
 from spyne.decorator import rpc
 from .complexmodels import Scenario,\
